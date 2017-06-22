6034db8973b0eb35987a1f600661bfce9bfdbefa
BK <bkam03@yahoo.com>
2017-06-21 21:01:23 -1000

    chirp functions

--- js/chirp.js
+++ js/chirp.js
@@ -1,5 +1,23 @@
+/*
+Calling chirp(3) will return the text "chirp chirp chirp"
+chirp will call itself, each time returning "chirp "
+*/
+
 function chirp(n){
-  // FIX ME
+
+  var string = '';
+
+  function recurse( n, string ){
+
+    if( n === 0 ){
+      console.log( 'end' );
+      return string;
+    } else {
+      string += 'chirp ';
+      return recurse( n-1, string );
+    }
+  }
+  return recurse(n, string);
 }
 
 $(document).ready(function(){
